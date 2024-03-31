1a3458ba33d84f2e69c53458e1da0f9973e12499
Gabriel Soares <gedex3d@gmail.com>
2024-03-30 21:36:48 -0300

    feat: configuring development proccess

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Controle de Finanças",
-  description: "Um app para control das nossas finanças",
+  title: 'Controle de Finanças',
+  description: 'Um app para control das nossas finanças',
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <form action="submit">
-
-      </form>
+      <form action="submit"></form>
     </main>
   );
 }
